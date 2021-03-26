9238539edcc3b90067aae8549225e48d395bed47
harshitadubey <harshitadubeybpl@gmail.com>
2021-03-26 13:08:16 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Node-Twilio Example
+
+It's help to implement twilio sms notification service and api's.
+
+## Installation
+
+Use the git [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+yarn install
+```
+
+## Usage
+
+```Config_ENV_Veriable
+export accountSid=Your_accountSid;
+export authToken=Your_authToken;
+export service_ssid=Your_service_ssid;
+
+```
+
+```yarn
+yarn run dev
+```
+
+## Contributing
+Harshita Dubey
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,27 @@
+const accountSid = process.env.accountSid; 
+const authToken = process.env.authToken;
+const service_ssid = process.env.service_ssid;
+// ** es6 **//
+// const client = require('twilio')(accountSid, authToken); 
+// client.messages 
+//       .create({ 
+//          body: 'Hello Node : Ambuj Dubey',  
+//          messagingServiceSid: service_ssid,      
+//          to: '+919753750000' 
+//        }) 
+//       .then(message => console.log('Hi finished',message.sid))      
+// 	  .catch(err => console.log('Error : ',err))
+//       .done();
+
+// ** OLD **//
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: 'Hello Node : Ambuj Dubey',
+    to: '+919753750000',  // Text this number
+    // from: '+15005550006' // From a valid Twilio number
+    messagingServiceSid: service_ssid,
+})
+.then((message) => console.log(message.sid))      
+.catch(err => console.log('Error : ',err));
